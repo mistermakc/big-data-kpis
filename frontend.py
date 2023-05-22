cb4cd40ccc89e990e8ae61ea0a1f011d71374569
Max Heilingbrunner <maxheilingbrunner@xMacBook.local>
2023-05-23 00:41:17 +0200

    dashboard v1.0

--- frontend.py
+++ frontend.py
@@ -1,25 +1,22 @@
 # Import necessary modules
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
 from PIL import Image
-import numpy as np
 import altair as alt
 
 # Setting theme for streamlit
 image = Image.open('data/images/logo.png')
 st.set_page_config(
-    page_title="Groceries Dashboard", 
+    page_title="Pepsico Inc. Dashboard", 
     page_icon=image, 
     layout="wide", 
     initial_sidebar_state="expanded",
 )
 
-# Title
+# Set the title
 st.title('Pepsico Inc. Dashboard')
 
-# Defining Streamlit widgets for filters
+# Define Streamlit widgets for filters
 st.sidebar.image("data/images/logo.png", output_format='PNG', use_column_width=True)
 
 # Load the data
@@ -34,7 +31,6 @@ def load_data():
         'ny_promo_pct_df': pd.read_csv('data/output/ny_promo_pct_df.csv'),
         'ny_top_products_df': pd.read_csv('data/output/ny_top_products_df.csv'),
         'yearly_totals_df': pd.read_csv('data/output/yearly_totals_df.csv'),
-        'vendor_revenue': pd.read_csv('data/output/vendor_revenue.csv'),
         'top_regions_df': pd.read_csv('data/output/top_regions_df.csv'),
     }
     return dataframes
@@ -48,10 +44,11 @@ ny_promo_pct_df = data['ny_promo_pct_df']
 ny_top_products_df = data['ny_top_products_df']
 yearly_totals_df = data['yearly_totals_df']
 top_regions_df = data['top_regions_df']
-vendor_revenue = data['vendor_revenue']
+ny_pepsico_vsod_df = data['ny_pepsico_vsod_df']
 
 # Get the unique years present in the DataFrame
 unique_years = sorted(list(ny_pepsico_df['YEAR'].unique()))
+default_option = unique_years[1]
 
 # Use the years as options for the multiselect widget
 selected_years = st.sidebar.multiselect(
@@ -60,6 +57,10 @@ selected_years = st.sidebar.multiselect(
     default=unique_years,
     key="filter_years"
 )
+
+if not selected_years:
+    st.warning('Please select at least one option.')
+    selection = default_option
 
 # Get the unique UPCs present in the DataFrame
 unique_upc = sorted(list(ny_pepsico_df['UPC'].unique()))
@@ -87,26 +88,29 @@ selected_retailer = st.sidebar.selectbox(
 )
 
 # Add a slider to select the number of top products to display
-selected_product_number = st.sidebar.slider("Number of top products to display", 1, 20, 10)
+selected_product_number = st.sidebar.slider("NO. PRODUCTS", 3, 20, 10)
 
 # Filter the DataFrame based on the selected years
 filter_ny_pepsico_df = ny_pepsico_df[ny_pepsico_df['YEAR'].isin(selected_years)]
 filter_market_share_df = market_share_df[market_share_df['YEAR'].isin(selected_years)]
 filter_top_regions_df = top_regions_df[top_regions_df['YEAR'].isin(selected_years)]
 filter_ny_top_products_df = ny_top_products_df[ny_top_products_df['YEAR'].isin(selected_years)]
+filter_ny_pepsico_vsod_df = ny_pepsico_vsod_df[ny_pepsico_vsod_df['YEAR'].isin(selected_years)]
+filter_ny_promo_pct_df = ny_promo_pct_df[ny_promo_pct_df['YEAR'].isin(selected_years)]
 
-# Calculate total volume and revenue for selected years
+# Calculate total volume, revenue and vsod for selected years
 total_volume = round(filter_ny_pepsico_df['UNITS'].sum(), 0)
 total_revenue = round(filter_ny_pepsico_df['DOLLARS'].sum(), 0)
+total_volume_sold_on_deal = round(filter_ny_pepsico_vsod_df['VSoD'].sum(), 0)
 
 # Calculate total market share
 total_market_share_pepsico = filter_market_share_df[filter_market_share_df['L3'] == 'PEPSICO INC']['DOLLARS'].sum()
 total_market_share_all = filter_market_share_df['DOLLARS'].sum()
 total_marketshare = (total_market_share_pepsico / total_market_share_all) * 100
 
-# Display total volume and revenue in Streamlit
+# Display key kpis
 with st.container():
-    kpi1, kpi2, kpi3 = st.columns(3)
+    kpi1, kpi2, kpi3, kpi4 = st.columns(4)
 
     kpi1.metric(
         label = "Total Revenue",
@@ -122,8 +126,12 @@ with st.container():
         label = "Total Market Share",
         value = "{:,.2f}%".format(total_marketshare),
     )
+    kpi4.metric(
+        label = "Total Volume Sold on Deal",
+        value = "{:,.0f}".format(total_volume_sold_on_deal),
+    )
 
-#####
+# CHART: TOP REGIONS
 
 # Group by 'Market_Name' and sum 'Total_Revenue'
 grouped_data = filter_top_regions_df.groupby('Market_Name')['Total_Revenue'].sum().reset_index()
@@ -151,47 +159,39 @@ chart_top_regions = alt.Chart(grouped_data).mark_bar(color='#1E4B92').encode(
     anchor='middle'
 )
 
-
-
-
-#### NEXT CHART
+# CHART: TOP RETAILERS
 
 # Group by MskdName and sum UNITS and DOLLARS columns
 ny_pepsico_customers_df = filter_ny_pepsico_df.groupby('MskdName').agg({'DOLLARS': 'sum', 'UNITS': 'sum'})
 
 # Sort by DOLLARS column in descending order
 ny_pepsico_top_customers_df = ny_pepsico_customers_df.sort_values('DOLLARS', ascending=False).reset_index()
-
-# Altair requires long format data
 ny_pepsico_top_customers_melt_df = ny_pepsico_top_customers_df.melt('MskdName', ['DOLLARS', 'UNITS'], 'Metric', 'Value')
 
 # Create bar chart (dollars)
-dollars_chart = alt.Chart(ny_pepsico_top_customers_melt_df[ny_pepsico_top_customers_melt_df['Metric']=='DOLLARS']).mark_bar().encode(
-    x=alt.X('MskdName:N'),
-    y=alt.Y('Value:Q', axis=alt.Axis(title='Dollars')),
+chart_retailers_revenue = alt.Chart(ny_pepsico_top_customers_melt_df[ny_pepsico_top_customers_melt_df['Metric']=='DOLLARS']).mark_bar().encode(
+    x=alt.X('MskdName:N', sort='-y'),
+    y=alt.Y('Value:Q', axis=alt.Axis(title='Revenue ($)')),
     color=alt.value('#1E4B92'),
 ).properties(
     title='Top Retailers by Revenue',
-    width=alt.Step(30)  # controls width of bar.
 )
 
 # Create line chart (units)
-units_chart = alt.Chart(ny_pepsico_top_customers_melt_df[ny_pepsico_top_customers_melt_df['Metric']=='UNITS']).mark_line().encode(
+chart_retailers_units = alt.Chart(ny_pepsico_top_customers_melt_df[ny_pepsico_top_customers_melt_df['Metric']=='UNITS']).mark_line().encode(
     x=alt.X('MskdName:N'),
-    y=alt.Y('Value:Q', axis=alt.Axis(title='Units')),
+    y=alt.Y('Value:Q', axis=alt.Axis(title='Units (#)')),
     color=alt.value('red'),
 )
 
 # Combine charts
-chart_top_retailers = alt.layer(dollars_chart, units_chart).resolve_scale(
-    y='independent'  # this makes y-axis independent for each chart
+chart_top_retailers = alt.layer(chart_retailers_revenue, chart_retailers_units).resolve_scale(
+    y='independent' 
 ).configure_title(
     anchor='middle'
 )
 
-
-
-##NEXT CHART
+# CHART: TOP PRODUCTS
 
 # Group by UPC and PRODUCT TYPE, and sum DOLLARS and UNITS
 ny_top_products_df = filter_ny_top_products_df.groupby(['UPC', 'PRODUCT TYPE']).agg({'DOLLARS': 'sum', 'UNITS': 'sum'}).reset_index()
@@ -204,8 +204,8 @@ ny_top_products_df['UPC_PRODUCT_TYPE'] = ny_top_products_df['UPC'] + " - " + ny_
 
 # Create Altair bar chart
 chart_top_products = alt.Chart(ny_top_products_df).mark_bar().encode(
-    x=alt.X('UPC_PRODUCT_TYPE:N', title='Product Type', sort='-y'),
-    y=alt.Y('DOLLARS:Q', title='Dollars'),
+    x=alt.X('UPC_PRODUCT_TYPE:N', title='UPC & Product Type', sort='-y'),
+    y=alt.Y('DOLLARS:Q', title='Revenue ($)'),
     color=alt.value('#1E4B92'),
 ).properties(
     title='Top Selling Products by Revenue'
@@ -213,22 +213,40 @@ chart_top_products = alt.Chart(ny_top_products_df).mark_bar().encode(
     anchor='middle'
 )
 
+# CHART: PROMOTION PER UNIT
 
+# Calculate weighted average of PR using UNITS as weights
+ny_promo_pct_df['WEIGHTED_PR'] = filter_ny_promo_pct_df['PR'] * filter_ny_promo_pct_df['UNITS']
+ny_promo_pct_df = ny_promo_pct_df.groupby('UPC').agg({'WEIGHTED_PR': 'sum', 'UNITS': 'sum'}).reset_index()
+ny_promo_pct_df['WEIGHTED_AVG_PR'] = ny_promo_pct_df['WEIGHTED_PR'] / ny_promo_pct_df['UNITS']
 
+# Sort by DOLLARS column in descending order and select the top N products
+ny_promo_pct_df = ny_promo_pct_df.sort_values('WEIGHTED_AVG_PR', ascending=False).head(selected_product_number)
 
+# Create Altair bar chart with filtered products
+chart_promo_pct = alt.Chart(ny_promo_pct_df).mark_bar().encode(
+    x=alt.X('UPC:N', title='UPC', sort='-y'),
+    y=alt.Y('WEIGHTED_AVG_PR:Q', title='Average PR (%)'),
+    color=alt.value('#1E4B92'),
+).properties(
+    title='Average Promotional Activity for Top Products'
+).configure_title(
+    anchor='middle'
+)
 
-
-### NEXT CHART
+# CHARTS: REVENUE, UNITS, AVERAGE PR AND MARKETING, AND AVERAGE PRICE
 
 # Filter the dataframe by item and New York area
-ny_pepsico_item_df = filter_ny_pepsico_df[(filter_ny_pepsico_df['MskdName'] == selected_retailer) & (filter_ny_pepsico_df['UPC'] == selected_upc)]
+ny_pepsico_item_df = filter_ny_pepsico_df[(filter_ny_pepsico_df['MskdName'] == selected_retailer) & (filter_ny_pepsico_df['UPC'] == selected_upc)].copy()
 
-# Convert 'YEAR' to datetime and set as index
-ny_pepsico_item_df['YEAR'] = pd.to_datetime(ny_pepsico_item_df['YEAR'], format='%Y')
-ny_pepsico_item_df.set_index('YEAR', inplace=True)
+# Convert 'Calendar week starting on' to datetime
+ny_pepsico_item_df['Calendar week starting on'] = pd.to_datetime(ny_pepsico_item_df['Calendar week starting on'])
 
-# Group the data by year and calculate the metrics
-ny_pepsico_item_weekly_df = ny_pepsico_item_df.groupby('YEAR').agg(
+# Set 'Calendar week starting on' as the index
+ny_pepsico_item_df.set_index('Calendar week starting on', inplace=True)
+
+# Group the data by week and calculate the metrics
+ny_pepsico_item_weekly_df = ny_pepsico_item_df.resample('W').agg(
     {'DOLLARS': 'sum', 'UNITS': 'sum', 'PR': 'mean', 'D': 'mean'}
 )
 ny_pepsico_item_weekly_df.columns = ['Revenue', 'Units Sold', 'Avg PR', 'Avg Marketing']
@@ -236,53 +254,104 @@ ny_pepsico_item_weekly_df.columns = ['Revenue', 'Units Sold', 'Avg PR', 'Avg Mar
 # Calculate the average price
 ny_pepsico_item_weekly_df['Avg Price'] = ny_pepsico_item_weekly_df['Revenue'] / ny_pepsico_item_weekly_df['Units Sold']
 
-# Set up altair chart base
-base = alt.Chart(ny_pepsico_item_weekly_df.reset_index()).encode(
-    alt.X('YEAR:T')
+# Reset the index for Altair plotting
+ny_pepsico_item_weekly_df = ny_pepsico_item_weekly_df.reset_index()
+
+# Base layer
+base = alt.Chart(ny_pepsico_item_weekly_df).encode(
+    alt.X('Calendar week starting on:T', title='Week')
 )
+
+# Select the boundaries
+def get_min_max_values(df):
+    if df.empty:
+        min_revenue = ""
+        max_revenue = ""
+        min_units_sold = ""
+        max_units_sold = ""
+    else:
+        min_revenue = df['Revenue'].min()
+        max_revenue = df['Revenue'].max()
+        min_units_sold = df['Units Sold'].min()
+        max_units_sold = df['Units Sold'].max()
+
+    return min_revenue, max_revenue, min_units_sold, max_units_sold
+
+min_revenue, max_revenue, min_units_sold, max_units_sold = get_min_max_values(ny_pepsico_item_weekly_df)
 
 # Define the charts
-chart1 = base.mark_line(color='blue').encode(
-    alt.Y('Revenue:Q', title='Metrics'),
-).properties(title=f"Pepsico's Revenue and Units Sold for Item {selected_upc} in NY for {selected_retailer}")
-
-chart2 = base.mark_line(color='green').encode(
-    alt.Y('Units Sold:Q', title='Metrics')
+chart_revenue = base.mark_line().encode(
+    alt.Y('Revenue:Q', axis=alt.Axis(title='Revenue ($)', titleColor='#1E4B92'), scale=alt.Scale(zero=False)),
+    color=alt.value('#1E4B92'),
+    tooltip=['Revenue', 'Calendar week starting on']
 )
 
-chart3 = base.mark_line(color='red').encode(
-    alt.Y('Avg PR:Q', title='Metrics')
-).properties(title=f"Pepsico's Avg PR and Avg Marketing for Item {selected_upc} in NY for {selected_retailer}")
-
-chart4 = base.mark_line(color='purple').encode(
-    alt.Y('Avg Marketing:Q', title='Metrics')
+chart_units = base.mark_line().encode(
+    alt.Y('Units Sold:Q', axis=alt.Axis(title='Units Sold (#)', titleColor='green'), scale=alt.Scale(domain=(min_units_sold, max_revenue))),
+    color=alt.value('green'),
+    tooltip=['Units Sold', 'Calendar week starting on']
 )
 
-chart5 = base.mark_line(color='orange').encode(
-    alt.Y('Avg Price:Q', title='Average Price')
-).properties(title=f"Pepsico's Average Price for Item {selected_upc} in NY for {selected_retailer}")
+# Combine chart_revenue and chart_units with independent Y scales
+chart_financials = alt.layer(chart_revenue, chart_units).resolve_scale(y='independent').properties(
+    title={"text": f"Revenue & Units Sold for Item {selected_upc} for {selected_retailer}", "anchor": "middle"},
+    width=600,
+    height=300
+)
+
+chart_average_pr = base.mark_line().encode(
+    alt.Y('Avg PR:Q', axis=alt.Axis(title='Average PR (%)', titleColor='red')),
+    color=alt.value('red'),
+    tooltip=['Avg PR', 'Calendar week starting on']
+)
+
+chart_average_marketing = base.mark_line().encode(
+    alt.Y('Avg Marketing:Q', axis=alt.Axis(title='Average Marketing (%)', titleColor='purple')),
+    color=alt.value('purple'),
+    tooltip=['Avg Marketing', 'Calendar week starting on']
+)
+
+# Combine chart_average_pr and chart_average_marketing with independent Y scales
+chart_advertising = alt.layer(chart_average_pr, chart_average_marketing).resolve_scale(y='independent').properties(
+    title={"text": f"Avg. PR & Marketing for Item {selected_upc} for {selected_retailer}", "anchor": "middle"},
+    width=600,
+    height=300
+)
+
+chart_average_price = base.mark_line().encode(
+    alt.Y('Avg Price:Q', axis=alt.Axis(title='Average Price ($)', titleColor='orange')),
+    color=alt.value('orange'),
+    tooltip=['Avg Price', 'Calendar week starting on']
+).properties(title={"text": f"Avg. Price for Item {selected_upc} for {selected_retailer}", "anchor": "middle"})
 
 
-# Creating tabs to display different KPIs
-tab1, tab2, tab3 = st.tabs(["Market", "New York", "Pepsico"])
+# Create tabs to display different KPIs
+tab1, tab2, tab3 = st.tabs(["Total Market", "New York", "Pepsico (New York)"])
 
 with tab1:
+    # Display the chart for top regions
     st.altair_chart(chart_top_regions, use_container_width=True)
 
 with tab2:
-    # Displaying the two charts side by side
+    # Display the chart for top retailers
+    st.altair_chart(chart_top_retailers, use_container_width=True)
+
+    # Display the two charts side by side
     col1, col2 = st.columns(2, gap="small")
     with col1:
-        st.altair_chart(chart_top_retailers, use_container_width=True)
+        # Display the chart for promotion per item
+        st.altair_chart(chart_promo_pct, use_container_width=True)
+        
     with col2:
+        # Display the chart for top performing products
         st.altair_chart(chart_top_products, use_container_width=True)
 
 with tab3:
-    # Displaying the charts for Revenue and Units Sold
-    st.altair_chart(chart1 + chart2, use_container_width=True)
+    # Display the charts for revenue and units sold
+    st.altair_chart(chart_financials, use_container_width=True)
 
-    # Displaying the charts for Avg PR and Avg Marketing
-    st.altair_chart(chart2, use_container_width=True)
+    # Display the charts for average PR and average marketing
+    st.altair_chart(chart_advertising, use_container_width=True)
 
-    # Displaying the chart for Avg Price
-    st.altair_chart(chart3, use_container_width=True)
+    # Display the chart for average price
+    st.altair_chart(chart_average_price, use_container_width=True)
